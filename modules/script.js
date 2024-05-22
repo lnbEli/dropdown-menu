ba8489e4df773ee24aab5b54e7dd96ff28d8882c
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-05-22 15:02:35 +0200

    moved script to module folder

---
+++ modules/script.js
@@ -0,0 +1,49 @@
+function dropDownMenu() {
+  console.log("works");
+  function toggleDropDownDisplayOnOff() {
+    const dropdown = document.querySelector(".drop-down-list");
+    dropdown.classList.toggle("visible");
+  }
+
+  function hideDropDown() {
+    const dropdown = document.querySelector(".drop-down-list");
+    if (dropdown) {
+      dropdown.classList.remove("visible");
+    }
+  }
+
+  function functionOne() {
+    console.log("one");
+  }
+  function functionTwo() {
+    console.log("two");
+  }
+  function functionThree() {
+    console.log("three");
+  }
+
+  const buttonFunctions = [functionOne, functionTwo, functionThree];
+
+  function addEventListenersToDropDownButtons() {
+    const threeDotButton = document.querySelector(".dots");
+    const dropdown = document.querySelector(".drop-down-list");
+    const dropdownButtons = document.querySelectorAll(".drop-down-items>li");
+
+    threeDotButton.addEventListener("click", toggleDropDownDisplayOnOff);
+
+    dropdown.addEventListener("mouseleave", hideDropDown);
+
+    dropdownButtons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        buttonFunctions[index]();
+        hideDropDown();
+      });
+    });
+  }
+  document.addEventListener(
+    "DOMContentLoaded",
+    addEventListenersToDropDownButtons
+  );
+}
+
+module.exports = dropDownMenu;
